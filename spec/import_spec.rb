28fbc05cb466af90e8b6c2d1f59fa3d5d5961b8f
Igor Fedoronchuk <igor.f@didww.com>
2015-01-17 02:06:58 +0200

    spec for zip import if not allowed

--- spec/import_spec.rb
+++ spec/import_spec.rb
@@ -1,12 +1,6 @@
 require 'spec_helper'
 
 describe 'import', type: :feature do
-  let(:options) { {} }
-
-  before do
-    add_author_resource(options)
-    visit '/admin/authors/import'
-  end
 
   def with_zipped_csv(name, &block)
 
@@ -29,113 +23,144 @@ describe 'import', type: :feature do
   end
 
 
-  it "has valid form" do
-    form = find('#new_active_admin_import_model')
-    expect(form['action']).to eq("/admin/authors/do_import")
-    expect(form['enctype']).to eq("multipart/form-data")
-    file_input = form.find("input#active_admin_import_model_file")
-    expect(file_input[:type]).to eq("file")
-    expect(file_input.value).to be_blank
-    submit_input = form.find("#active_admin_import_model_submit_action input")
-    expect(submit_input[:value]).to eq("Import")
-    expect(submit_input[:type]).to eq("submit")
-  end
+  context "with valid options" do
 
-  context "with hint defined" do
-    let(:options) {
-      {template_object: ActiveAdminImport::Model.new(hint: "hint")}
-    }
-    it "renders hint at upload page" do
-      expect(page).to have_content options[:template_object].hint
-    end
+    let(:options) { {} }
 
-  end
-
-
-  context "import file" do
-
-    [:empty, :only_headers].each do |file|
-      context "when #{file} file" do
-        it "should render warning" do
-
-          upload_file!(file)
-          expect(page).to have_content I18n.t('active_admin_import.file_empty_error')
-          expect(Author.count).to eq(0)
-        end
-      end
-    end
-
-    context "when no file" do
-      it "should render error" do
-        find_button('Import').click
-        expect(Author.count).to eq(0)
-        expect(page).to have_content I18n.t('active_admin_import.no_file_error')
-      end
-    end
-
-    context "with headers" do
-
-      it "should import file with many records" do
-        upload_file!(:authors)
-        expect(page).to have_content "Successfully imported 2 authors"
-        expect(Author.count).to eq(2)
-      end
-
-      it "should import file with 1 record" do
-        upload_file!(:author)
-        expect(page).to have_content "Successfully imported 1 author"
-        expect(Author.count).to eq(1)
-      end
+    before do
+      add_author_resource(options)
+      visit '/admin/authors/import'
     end
 
 
-    context "without headers" do
-      context "with known csv headers" do
-        before do
-          allow_any_instance_of(ActiveAdminImport::Model).to receive(:csv_headers).and_return(['Name', 'Last name', 'Birthday'])
-        end
-
-        it "should import file" do
-          upload_file!(:authors_no_headers)
-          expect(page).to have_content "Successfully imported 2 authors"
-          expect(Author.count).to eq(2)
-        end
-      end
-
-      context "with unknown csv headers" do
-        it "should render error" do
-          upload_file!(:authors_no_headers)
-          expect(page).to have_content "Error:"
-          expect(Author.count).to eq(0)
-        end
-      end
-
+    it "has valid form" do
+      form = find('#new_active_admin_import_model')
+      expect(form['action']).to eq("/admin/authors/do_import")
+      expect(form['enctype']).to eq("multipart/form-data")
+      file_input = form.find("input#active_admin_import_model_file")
+      expect(file_input[:type]).to eq("file")
+      expect(file_input.value).to be_blank
+      submit_input = form.find("#active_admin_import_model_submit_action input")
+      expect(submit_input[:value]).to eq("Import")
+      expect(submit_input[:type]).to eq("submit")
     end
 
-
-    context "when zipped" do
-      it "should import file" do
-        with_zipped_csv(:authors) do
-          upload_file!(:authors, :zip)
-          expect(page).to have_content "Successfully imported 2 authors"
-          expect(Author.count).to eq(2)
-        end
-      end
-    end
-
-
-    context "with semicolons separator" do
+    context "with hint defined" do
       let(:options) {
-        {template_object: ActiveAdminImport::Model.new(csv_options: {col_sep: ";"})}
+        {template_object: ActiveAdminImport::Model.new(hint: "hint")}
       }
-      it "should import file" do
-        upload_file!(:authors_with_semicolons)
-        expect(page).to have_content "Successfully imported 2 authors"
-        expect(Author.count).to eq(2)
+      it "renders hint at upload page" do
+        expect(page).to have_content options[:template_object].hint
       end
+
+    end
+
+
+    context "when importing file" do
+
+      [:empty, :only_headers].each do |file|
+        context "when #{file} file" do
+          it "should render warning" do
+
+            upload_file!(file)
+            expect(page).to have_content I18n.t('active_admin_import.file_empty_error')
+            expect(Author.count).to eq(0)
+          end
+        end
+      end
+
+      context "when no file" do
+        it "should render error" do
+          find_button('Import').click
+          expect(Author.count).to eq(0)
+          expect(page).to have_content I18n.t('active_admin_import.no_file_error')
+        end
+      end
+
+      context "with headers" do
+
+        it "should import file with many records" do
+          upload_file!(:authors)
+          expect(page).to have_content "Successfully imported 2 authors"
+          expect(Author.count).to eq(2)
+        end
+
+        it "should import file with 1 record" do
+          upload_file!(:author)
+          expect(page).to have_content "Successfully imported 1 author"
+          expect(Author.count).to eq(1)
+        end
+      end
+
+
+      context "without headers" do
+        context "with known csv headers" do
+          before do
+            allow_any_instance_of(ActiveAdminImport::Model).to receive(:csv_headers).and_return(['Name', 'Last name', 'Birthday'])
+          end
+
+          it "should import file" do
+            upload_file!(:authors_no_headers)
+            expect(page).to have_content "Successfully imported 2 authors"
+            expect(Author.count).to eq(2)
+          end
+        end
+
+        context "with unknown csv headers" do
+          it "should render error" do
+            upload_file!(:authors_no_headers)
+            expect(page).to have_content "Error:"
+            expect(Author.count).to eq(0)
+          end
+        end
+
+      end
+
+
+      context "when zipped" do
+        context "when allowed" do
+
+          it "should import file" do
+            with_zipped_csv(:authors) do
+              upload_file!(:authors, :zip)
+              expect(page).to have_content "Successfully imported 2 authors"
+              expect(Author.count).to eq(2)
+            end
+          end
+
+        end
+
+        context "when not allowed" do
+          let(:options) { {
+              template_object: ActiveAdminImport::Model.new(allow_archive: false)
+          } }
+
+          it "should render error" do
+            with_zipped_csv(:authors) do
+              upload_file!(:authors, :zip)
+              expect(page).to have_content I18n.t('active_admin_import.file_format_error')
+              expect(Author.count).to eq(0)
+            end
+          end
+        end
+      end
+
+
+      context "with semicolons separator" do
+        let(:options) {
+          {template_object: ActiveAdminImport::Model.new(csv_options: {col_sep: ";"})}
+        }
+        it "should import file" do
+          upload_file!(:authors_with_semicolons)
+          expect(page).to have_content "Successfully imported 2 authors"
+          expect(Author.count).to eq(2)
+        end
+      end
+
     end
 
   end
+
 
 
 end
