191ba642f232585e1081cdc571f48d83ef30f103
Igor Fedoronchuk <igor.f@didww.com>
2015-01-17 21:57:10 +0200

    refactoring

--- lib/active_admin_import/options.rb
+++ lib/active_admin_import/options.rb
@@ -1,6 +1,7 @@
 module ActiveAdminImport
 
-  VALID_OPTIONS = [
+  module Options
+    VALID_OPTIONS = [
         :back,
         :csv_options,
         :validate,
@@ -16,12 +17,27 @@ module ActiveAdminImport
         :template_object,
         :resource_class,
         :resource_label,
-        :plural_resource_label ,
+        :plural_resource_label,
         :headers_rewrites
-      ].freeze
+    ].freeze
+
+
+    def self.options_for(config, options= {})
+      options[:template_object] = ActiveAdminImport::Model.new  unless options.has_key? :template_object
+
+      {
+          back: {action: :import},
+          csv_options: {},
+          template: "admin/import",
+          resource_class: config.resource_class,
+          resource_label: config.resource_label,
+          plural_resource_label: config.plural_resource_label,
+          headers_rewrites: {}
+      }.deep_merge(options)
 
 
 
+    end
 
-
+  end
 end
