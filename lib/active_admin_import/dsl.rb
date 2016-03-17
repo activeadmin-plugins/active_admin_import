70c5f7cce955dbcff7ac474a8fe9a0689e21094a
Anton Fefilov <anton.fefilov@macrocoders.com>
2016-03-17 12:28:26 +0400

    fix Rails5 related issues
    * "no file to import" issue fixed
    * "no method error" on type_cast method fixed

--- lib/active_admin_import/dsl.rb
+++ lib/active_admin_import/dsl.rb
@@ -65,7 +65,7 @@ module ActiveAdminImport
         authorize!(ActiveAdminImport::Auth::IMPORT, active_admin_config.resource_class)
 
         @active_admin_import_model = options[:template_object]
-        @active_admin_import_model.assign_attributes(params[params_key].try(:deep_symbolize_keys) || {})
+        @active_admin_import_model.assign_attributes(params[params_key].to_unsafe_h.deep_symbolize_keys) if params[params_key]
         #go back to form
         return render template: options[:template] unless @active_admin_import_model.valid?
         @importer = Importer.new(options[:resource_class], @active_admin_import_model, options)
